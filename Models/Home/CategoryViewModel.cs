c00a6b005bebfc80d68e9938f09fe17989792b81
ShaimaaGhassan <shimamadi2005@gmail.com>
2025-05-16 20:27:12 +0300

    complete the views of home page.

---
+++ Models/Home/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Html2ViewsConversion.Models.Home
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string IconClass { get; set; }
+        public string Description { get; set; }
+    }
+}
